a0bf1b8ec3be715991974b3dc3dbb1523595291e
Tushar waila <tusharwaila862@gmail.com>
2025-04-28 12:23:12 +0530

    Create goto_assertion.sv

---
+++ goto_assertion.sv
@@ -0,0 +1,48 @@
+module tb;
+bit req;
+bit grant;
+bit clk=0;
+
+always #5 clk = !clk;
+
+property ppt;
+@(posedge clk)
+req |=>grant[->3]##1(!req); //in goto assertion (!req) should be immidiately followed when consequent becomes true. 
+endproperty
+
+acc :assert property(ppt)
+     $display("-----------------------pass-----------------------",$time);
+	 else
+	 $display("-----------------------fail-----------------------",$time);
+
+initial begin
+#5;
+req=1;
+#10;
+grant=1;
+#10;
+grant=0;
+#10;
+grant=1;
+#10;
+grant=0;
+#10;
+grant=1;
+#10;
+grant=0;
+req=0;
+#200;
+$finish();
+end
+
+
+initial begin
+$dumpfile("nonconsecutive.vcd");
+$dumpvars();
+end
+
+initial begin
+$recordfile("nonconsecutive.trn");
+$recordvars();
+end
+endmodule
