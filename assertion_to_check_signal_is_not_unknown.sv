c5540f2bc7d768d51ceecc5730d741e52613ad85
Tushar waila <tusharwaila862@gmail.com>
2025-05-27 16:57:35 +0530

    Update assertion_to_check_signal_is_not_unknown.sv

--- assertion_to_check_signal_is_not_unknown.sv
+++ assertion_to_check_signal_is_not_unknown.sv
@@ -1,32 +1,43 @@
 //without using !$isunknown how to write assertion to check the same thing
 module tb;
 bit clk=0;
- logic a;
+ logic[3:0] a;
   always #5 clk=!clk;
   
   initial begin
-  #5 a='b1;
-  #10 a='bx;
-  #10 a='b0;
-  #10 a='bz;
-  #10 a='b1;
-  #10 a='bx;
+  #5 a=4'b1011;
+  #10 a=4'b10xz;
+  #10 a=4'b1101;
+  #10 a=4'b1z01;
+  #10 a=4'b1010;
+  #10 a=4'b101x;
   end
 
   property ppt;
     @(posedge clk)
-    $countbits(a,'bx,'bz)==0;
+   ( (^(a)==0)||(^(a)==1)) ;
+ //  $countbits(a,'bx,'bz)==0;
+// !$isunknown(a);
   endproperty
   
   acc:assert property(ppt)
-    $display("--------------------bhot badiya beta---------------------------");
+    $display("--------------------passsss---------------------------");
     else
-      $display("------------------galat hai faill------------------------------------");
+      $display("------------------fail------------------------------------");
+
 initial begin
-  $dumpfile("clk.vcd");
+  $dumpfile("tb.vcd");
   $dumpvars;
-  #200;
-  $finish();
+end
+
+initial begin
+$recordfile("tb.trn");
+$recordvars();
+end
+
+initial begin
+#150;
+$finish();
 end
 endmodule
     
